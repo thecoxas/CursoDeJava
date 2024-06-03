da4225081a8dff3849006806300c193f6b923cb0
Dev theCoxas <168282052+thecoxas@users.noreply.github.com>
2024-06-03 15:53:38 -0300

    aula java curso em video

---
+++ Projetos do curso Java/CursoJava/src/AulaCursoEmVideo/DataHora.java
@@ -0,0 +1,13 @@
+package AulaCursoEmVideo;
+
+import java.util.Date;
+
+public class DataHora {
+    public static void main(String[] args) {
+        Date data = new Date();
+        System.out.println("A hora e data do sistema é:");
+        System.out.println(data.toString());
+        
+    }
+
+}
---
+++ Projetos do curso Java/CursoJava/src/AulaCursoEmVideo/Idioma.java
@@ -0,0 +1,12 @@
+package AulaCursoEmVideo;
+
+import java.util.Locale;
+
+public class Idioma {
+    public static void main(String[] args) {
+        Locale idioma =Locale.getDefault();
+        System.out.println("O idioma do meu Sistema é: ");
+        System.out.println(idioma);
+    }
+
+}
