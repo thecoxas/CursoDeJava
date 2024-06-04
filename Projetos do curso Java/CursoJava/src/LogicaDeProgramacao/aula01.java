1e24031a6cd4fe95043aa372a0c79a95729e7b7b
Dev theCoxas <168282052+thecoxas@users.noreply.github.com>
2024-06-03 21:52:25 -0300

    aula01 algoritmo

---
+++ Projetos do curso Java/CursoJava/src/LogicaDeProgramacao/aula01.java
@@ -0,0 +1,19 @@
+package LogicaDeProgramacao;
+
+public class aula01 {
+    public static void main(String[] args) {
+        // Variaveis e Tipos primitivos;
+
+        byte idade = 39;
+        long cpf = 123456789;
+        char sexo = 'M';
+        String nome = "Welton";
+        int filhos = 4;
+        double altura = 1.69; 
+        float salario = 2.200f;
+        boolean doador = true;
+
+        System.out.println("Eu "+nome+" tenho "+idade+" anos "+filhos+" filhos "+altura+" de altura ganho $"+salario+" de salario meu cpf é "+cpf+" se sou doador "+doador+ " meu sexo "+sexo );
+    }
+
+}
